1b1b68e8efaa1a51cc90305486b53cd890d3e58f
Peter Munch <peterrmuench@gmail.com>
2023-11-20 15:15:10 +0100

    Implement system matrix

--- tests/tp_01.cc
+++ tests/tp_01.cc
@@ -1,5 +1,6 @@
 #include <deal.II/lac/block_vector.h>
 #include <deal.II/lac/solver_gmres.h>
+#include <deal.II/lac/sparse_matrix.h>
 
 using namespace dealii;
 
@@ -7,16 +8,40 @@ template <typename Number>
 class SystemMatrix
 {
 public:
-  using VectorType = BlockVector<Number>;
+  using VectorType      = Vector<Number>;
+  using BlockVectorType = BlockVector<Number>;
+
+  SystemMatrix(const SparseMatrix<Number> &K,
+               const SparseMatrix<Number> &M,
+               const FullMatrix<Number>   &A_inv)
+    : K(K)
+    , M(M)
+    , A_inv(A_inv)
+  {}
 
   void
-  vmult(VectorType &dst, const VectorType &src) const
+  vmult(BlockVectorType &dst, const BlockVectorType &src) const
   {
-    (void)dst;
-    (void)src;
+    const unsigned int n_blocks = src.n_blocks();
+
+    VectorType tmp;
+    tmp.reinit(src.block(0));
+    for (unsigned int i = 0; i < n_blocks; ++i)
+      K.vmult(dst.block(i), src.block(i));
+
+    for (unsigned int i = 0; i < n_blocks; ++i)
+      {
+        M.vmult(tmp, src.block(i));
+
+        for (unsigned int j = 0; j < n_blocks; ++j)
+          dst.block(j).add(A_inv(j, i), tmp);
+      }
   }
 
 private:
+  const SparseMatrix<Number> &K;
+  const SparseMatrix<Number> &M;
+  const FullMatrix<Number>   &A_inv;
 };
 
 
@@ -51,15 +76,22 @@ test()
 
   VectorType x, rhs;
 
-  SystemMatrix<Number>   matrix;
+  SparseMatrix<Number> K, M;  // TODO: fill
+  FullMatrix<Number>   A_inv; //
+
+  SystemMatrix<Number>   matrix(K, M, A_inv);
   Preconditioner<Number> preconditioner;
 
   solver.solve(matrix, x, rhs, preconditioner);
 }
 
+
+
 int
 main()
 {
+  return 0;
+
   const int dim = 2;
 
   test<dim>();
