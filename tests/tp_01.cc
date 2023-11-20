eb31c3ecb2988596cb76400bdc6889ed96901711
Peter Munch <peterrmuench@gmail.com>
2023-11-20 15:43:30 +0100

    Implement patch precon

--- tests/tp_01.cc
+++ tests/tp_01.cc
@@ -1,6 +1,9 @@
+#include <deal.II/dofs/dof_handler.h>
+
 #include <deal.II/lac/block_vector.h>
 #include <deal.II/lac/solver_gmres.h>
 #include <deal.II/lac/sparse_matrix.h>
+#include <deal.II/lac/sparse_matrix_tools.h>
 
 using namespace dealii;
 
@@ -50,16 +53,89 @@ template <typename Number>
 class Preconditioner
 {
 public:
-  using VectorType = BlockVector<Number>;
+  using VectorType      = Vector<Number>;
+  using BlockVectorType = BlockVector<Number>;
+
+  template <int dim>
+  Preconditioner(const SparseMatrix<Number> &K,
+                 const SparseMatrix<Number> &M,
+                 const FullMatrix<Number>   &A_inv,
+                 const DoFHandler<dim>      &dof_handler)
+  {
+    for (const auto &cell : dof_handler.active_cell_iterators())
+      {
+        std::vector<types::global_dof_index> my_indices(
+          cell->get_fe().n_dofs_per_cell());
+        cell->get_dof_indices(my_indices);
+
+        indices.emplace_back(my_indices);
+      }
+
+    std::vector<FullMatrix<Number>> K_blocks, M_blocks;
+
+    SparseMatrixTools::restrict_to_full_matrices(K,
+                                                 K.get_sparsity_pattern(),
+                                                 indices,
+                                                 K_blocks);
+    SparseMatrixTools::restrict_to_full_matrices(M,
+                                                 M.get_sparsity_pattern(),
+                                                 indices,
+                                                 M_blocks);
+
+    blocks.resize(K_blocks.size());
+
+    for (unsigned int i = 0; i < blocks.size(); ++i)
+      {
+        const auto &K = K_blocks[i];
+        const auto &M = M_blocks[i];
+        auto       &B = blocks[i];
+
+        B = FullMatrix<Number>(K.m() * A_inv.m(), K.n() * A_inv.n());
+
+        for (unsigned int i = 0; i < A_inv.m(); ++i)
+          for (unsigned int j = 0; j < A_inv.n(); ++j)
+            for (unsigned int k = 0; k < K.m(); ++k)
+              for (unsigned int l = 0; l < K.n(); ++l)
+                B(k + i * K.m(), l + j * K.n()) =
+                  A_inv(i, j) * M(k, l) + ((i == j) ? K(k, l) : 0.0);
+
+        B.gauss_jordan();
+      }
+  }
 
   void
-  vmult(VectorType &dst, const VectorType &src) const
+  vmult(BlockVectorType &dst, const BlockVectorType &src) const
   {
-    (void)dst;
-    (void)src;
+    dst = 0.0;
+
+    Vector<Number> dst_local;
+    Vector<Number> src_local;
+
+    const unsigned int n_blocks = src.n_blocks();
+
+    for (unsigned int i = 0; i < blocks.size(); ++i)
+      {
+        // gather
+        src_local.reinit(blocks[i].m());
+        dst_local.reinit(blocks[i].m());
+
+        for (unsigned int b = 0, c = 0; b < n_blocks; ++b)
+          for (unsigned int j = 0; j < indices[i].size(); ++j, ++c)
+            src_local[c] = src.block(b)[indices[i][j]];
+
+        // patch solver
+        blocks[i].vmult(dst_local, src_local);
+
+        // scatter
+        for (unsigned int b = 0, c = 0; b < n_blocks; ++b)
+          for (unsigned int j = 0; j < indices[i].size(); ++j, ++c)
+            dst.block(b)[indices[i][j]] += dst_local[c];
+      }
   }
 
 private:
+  std::vector<std::vector<types::global_dof_index>> indices;
+  std::vector<FullMatrix<Number>>                   blocks;
 };
 
 
@@ -71,6 +147,8 @@ test()
   using Number     = double;
   using VectorType = BlockVector<Number>;
 
+  DoFHandler<dim> dof_handler;
+
   SolverControl           solver_control;
   SolverGMRES<VectorType> solver(solver_control);
 
@@ -80,7 +158,7 @@ test()
   FullMatrix<Number>   A_inv; //
 
   SystemMatrix<Number>   matrix(K, M, A_inv);
-  Preconditioner<Number> preconditioner;
+  Preconditioner<Number> preconditioner(K, M, A_inv, dof_handler);
 
   solver.solve(matrix, x, rhs, preconditioner);
 }
